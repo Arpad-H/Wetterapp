d2bb28d33510fc035afab266d9a1146b5c9a6d60
WolfyTheMighty <s48915@beuth-hochschule.de>
2024-02-01 18:28:25 +0100

    init

---
+++ src/main/java/org/oaad/wetterapp/WetterappApplication.java
@@ -0,0 +1,13 @@
+package org.oaad.wetterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetterappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/oaad/wetterapp/WetterappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oaad.wetterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
